480ef806513a12d26792071d7f40ca2a75a6e8db
kartiksathwara <kartiksathwara64@gmail.com>
2025-11-21 11:25:44 +0530

    1st intallation

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="text-5xl text-blue-900 font-bold">
+        Tailwind Working!
+      </div>
+
+    </>
+  )
+}
+
+export default App
